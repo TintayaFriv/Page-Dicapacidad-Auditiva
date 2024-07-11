a2c4412ec7ace1373ab37f3da50c746cb4b383ab
oscar-johao <144861978+oscar-johao@users.noreply.github.com>
2024-07-11 09:05:21 -0500

    se agrega el proyecto

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="diseño.css">
+    <title>Perdida Auditiva</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">¿Qué es la Discapacidad Auditiva?</a></li>
+                    <li><a href="#">Tratamientos</a></li>
+                    <li><button type="button">Donacion</button></li>
+                </ul>
+            </div>        
+        </nav>
+    </header>
+
+
+    <section class="content">
+        <p>En el Péru exiten más de 500 mil personas con discapcidad auditiva. El ultimo senso echo por el Instituto
+            nacional de Estadisitica e Información. En todo Lima Metropolitana, se confirmo que 144mil personas.</p>
+
+        <p>Y como cual quier persona con dispacidad tiene acceso a la información, ya que estan protegidas por la norma
+            legal número 29973. Permitiendo el acceso a las tecnologias de comunicación e información.</p>
+
+        <img src="img/discapacidad-auditiva-04.png" alt="discapacidad-auditiva-04">
+    </section>
+
+    <section>
+        ¿Qués es la discapacidad Auditiva?
+
+        <img src="img/oido.jpg" alt="oido">
+        <p>El Oido:</p>
+        <p>El oido tiene 3 partes: oido externo, odio medio y oido interno.</p>
+        <p>El oído externo tiene la funcion de captar el sonido y pasarlo al oído medio.
+
+            El oído medio recibe el sonido, las pasa por una membrana timpática y la pasa por los huesos pequeños (martillo - yunque - estribo). su funcion es trasportar el sonido a al oído interno.
+            
+            El oído interno lo forma el cóclea (se encarga de mandar el sonido al cerebro) y el vestíbulo (se encarga del equilibrio).</p>
+        
+
+    </section>
+    <script src="codigo.js"></script>
+</body>
+
+</html>
